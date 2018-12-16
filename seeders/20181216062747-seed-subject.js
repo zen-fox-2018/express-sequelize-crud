5dfb60ee7f143a6a606ffc6f1c2bc69925ec551d
taqiabdulaziz <svx.annexiv@gmail.com>
2018-12-16 14:39:09 +0700

    komit

---
+++ seeders/20181216062747-seed-subject.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+        return queryInterface.bulkInsert('Subjects', [{
+          subject_name: 'Kimia',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },{
+          subject_name: 'Ekonomi',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+     return queryInterface.bulkDelete('Subjects', null, {});
+  }
+};
