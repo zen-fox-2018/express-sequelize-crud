4280db78b91c2ca4b344e22e17e316578bf4769e
Celyn Susanto <celyn.vy@gmail.com>
2018-12-16 12:42:30 +0700

    Submit

--- models/student.js
+++ models/student.js
@@ -3,7 +3,30 @@ module.exports = (sequelize, DataTypes) => {
   const Student = sequelize.define('Students', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail : true,
+        isUnique: function(theEmail, next) {
+          let self = this;
+          Student
+          .findOne({
+            where: {
+              email: theEmail
+            }
+          })
+          .then(function(student) {
+            if (student && self.id != student.id) {
+              return next("Email has been used");
+            }
+            return next();
+          })
+          .catch(function(err) {
+            return next(err)
+          })
+        }
+      }
+    }
   }, {});
   Student.associate = function(models) {
     // associations can be defined here
--- models/teacher.js
+++ models/teacher.js
@@ -3,7 +3,30 @@ module.exports = (sequelize, DataTypes) => {
   const Teacher = sequelize.define('Teachers', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email:DataTypes.STRING
+    email:{
+      type: DataTypes.STRING,
+      validate: {
+        isEmail : true,
+        isUnique: function(theEmail, next) {
+          let self = this;
+          Teacher
+          .findOne({
+            where: {
+              email: theEmail
+            }
+          })
+          .then(function(teacher) {
+            if (teacher && self.id != teacher.id) {
+              return next("Email has been used");
+            }
+            return next();
+          })
+          .catch(function(err) {
+            return next(err)
+          })
+        }
+      }
+    }
   }, {});
   Teacher.associate = function(models) {
     // associations can be defined here
